b0b2abf0e23fc2701752164da1d17062662542a5
Shrijit Pendse <shrijitpendse@gmail.com>
2022-04-15 14:54:27 -0700

    Add files via upload

---
+++ Final_project.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 14:30:12 2022
+
+@author: Lenovo
+"""
+
+import tensorflow as tf
+from tensorflow.compat.v1.keras.preprocessing.text import Tokenizer
+from tensorflow.compat.v1.keras.preprocessing.sequence import pad_sequences
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+model = tf.keras.models.load_model("/Final_model.h5")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods = ["POST"])
+def predict():
+    tweet = request.form["tweet"]
+    processed_tweet = preprocess(tweet)
+    model_list = model.predict(processed_tweet)
+    
+    return render_template("index.html",pred_text = "The tweet is {}".format(model_list.mean()))
+    
+
+
+
+def preprocess(tweet):
+    tokenizer_test = Tokenizer()
+    sample = tweet
+    tokenizer_test.fit_on_texts(sample) 
+    max_length = 750 
+    # define vocabulary size
+    sample_tokens =  tokenizer_test.texts_to_sequences(sample)
+    samples_tokens_pad = pad_sequences(sample_tokens, maxlen=max_length, padding='post')
+    return samples_tokens_pad
+
+if __name__=='__main__':
+    app.run()
